6239b154bd6d4a6286c182d805bc45853ab95870
23yuli23 <virasetiyawan@gmail.com>
2026-07-23 00:24:00 +0700

    fix(auth): restrict customer transactions by role

--- routes/transaction.routes.ts
+++ routes/transaction.routes.ts
@@ -4,25 +4,25 @@ import {
   getTransactionByIdController,
   uploadPaymentProofController,
 } from "../controllers/transaction.controller.js";
-import { verifyToken } from "../middlewares/auth.middleware.js";
+import { verifyRole, verifyToken } from "../middlewares/auth.middleware.js";
 import { upload } from "../middlewares/upload.middleware.js";
 import { validateBody } from "../middlewares/validate.middleware.js";
 import { createTransactionSchema } from "../validators/transaction.validator.js";
 
 export const transactionRoutes = Router();
 
+transactionRoutes.use(verifyToken, verifyRole("CUSTOMER"));
+
 transactionRoutes.post(
   "/",
-  verifyToken,
   validateBody(createTransactionSchema),
   createTransactionController,
 );
 
-transactionRoutes.get("/:id", verifyToken, getTransactionByIdController);
+transactionRoutes.get("/:id", getTransactionByIdController);
 
 transactionRoutes.patch(
   "/:id/payment-proof",
-  verifyToken,
   upload(5).single("paymentProof"),
   uploadPaymentProofController,
 );
